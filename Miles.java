f6e808b21dbbaabcf4b99a496ea6c8f1d15a144a
Olga Belianko <Obelyanko@gmail.com>
2021-05-29 19:30:29 +0300

    miles.java update

--- Miles.java
+++ Miles.java
@@ -1,9 +1,9 @@
  public class BonusMilesService {
- public int calculate (int cost) {
- int price = 20_000; //в рублях
- int amount = 20; // rub
- int totalBonus = price / amount;
+   public int calculate (int cost) {
+   int price = 20_000; //в рублях
+   int amount = 20; // rub
+   int totalBonus = price / amount;
 
- return totalBonus;
- } 
+   return totalBonus;
+   } 
  }
